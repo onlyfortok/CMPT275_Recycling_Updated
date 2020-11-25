f1effb96e7a76933a9a3517f1c15388bafa54714
onlyfortok <onlyfortok@gmail.com>
2020-11-24 21:08:21 -0700

    updated

--- src/GUI/PlayGui.java
+++ src/GUI/PlayGui.java
@@ -1,9 +1,14 @@
 package GUI;
 
 import Game.GamePlayScreen;
+import Game.Game_Info;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 public class PlayGui implements GamePlayScreen {
@@ -12,14 +17,47 @@ public class PlayGui implements GamePlayScreen {
     private boolean play_again = false;
     private int score = 0;
 
+
     private JPanel main_panel;
     private JButton prevButton;
     private JPanel RecycleBin_1;
     private JPanel word_or_image_panel;
     private JButton nextButton;
+    private  JPanel panel=new JPanel(); //This panel will hold all the containers
+    private  JPanel panel2=new JPanel(); //This panel will hold all the radio buttons
+
+    private JLabel Word_Image= new JLabel();
+    private static JRadioButton b1 = new JRadioButton("Bottles or Cans");
+    private static JRadioButton b2 = new JRadioButton("E-Waste");
+    private static JRadioButton b3 = new JRadioButton("Mixed Paper");
+    private static JRadioButton b4 = new JRadioButton("Organics");
+    private static JRadioButton b5 = new JRadioButton("Plastics");
+    private static JRadioButton b6 = new JRadioButton("Trash");
+    private static ButtonGroup bg = new ButtonGroup();
+
+    private Game_Info G1 = new Game_Info();
+    private String word;
+
+
 
     public PlayGui(){
+        setting_containers(); //Setting the containers in the Gui
+        setting_Radio_buttons();//Setting the radio buttons in the Gui
 
+        nextButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Give_userInput_for_ScoreChecking();//Check if user has gotten correct answer
+                G1.increase_counter(); //go to next word
+                System.out.println( G1.get_score());
+                display_word_from_database();
+            }
+        });
+
+        word_or_image_panel.add(Word_Image);
+        word_or_image_panel.setBackground(Color.YELLOW);
+        frame.add(panel2);
+        frame.add(panel);
         frame.add(main_panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setPreferredSize(new Dimension(650, 600));
@@ -27,6 +65,95 @@ public class PlayGui implements GamePlayScreen {
         frame.setVisible(true);
 
     }
+
+    //Based on counter in Gameinfo class this word will display the word
+    public void display_word_from_database(){
+        String Game_difficulty_chosen = G1.getGame_Chosen();
+        if(Game_difficulty_chosen.equals("Hard")){
+            word = G1.get_Hard_database_word(G1.get_counter()); //G1 will store the counter to increment the scores and current position in array
+        }
+        else if(Game_difficulty_chosen.equals("Normal")){
+            word = G1.get_Normal_database_word(G1.get_counter()); //G1 will store the counter to increment the scores and current position in array
+        }
+        else{ //easy mode
+            word = G1.get_Easy_database_word(G1.get_counter()); //G1 will store the counter to increment the scores and current position in array
+        }
+        Word_Image.setText(word);
+    }
+
+    //Set Score
+    public void Give_userInput_for_ScoreChecking(){
+        if(b1.isSelected()){
+            G1.set_Current_user_word("Bottles/Cans");
+        }
+        if(b2.isSelected()){
+            G1.set_Current_user_word("E-Waste");
+        }
+        if(b3.isSelected()){
+            G1.set_Current_user_word("Mixed Paper");
+        }
+        if(b4.isSelected()){
+            G1.set_Current_user_word("Organics");
+        }
+        if(b5.isSelected()){
+            G1.set_Current_user_word("Plastics");
+        }
+        if(b6.isSelected()){
+            G1.set_Current_user_word("Trash");
+        }
+        G1.check_answer(); //checks the answer and increases score if it is corret
+    }
+
+
+
+    //Setting the radio buttons in the Gui
+    public void setting_Radio_buttons(){
+
+        panel2.setBounds(20,450,600,300);
+        panel2.add(b1);bg.add(b1);
+        panel2.add(b2);bg.add(b2);
+        panel2.add(b3);bg.add(b3);
+        panel2.add(b4);bg.add(b4);
+        panel2.add(b5);bg.add(b5);
+        panel2.add(b6);bg.add(b6);
+    }
+    //this function specifies the location of the containers
+    public void setting_containers(){
+
+        panel.setBounds(20,350,600,300);
+        ImageIcon img1 = resize_containers("src/Bins/Bottles-Cans.jpg");
+        ImageIcon img2 = resize_containers("src/Bins/E-Waste.jpg");
+        ImageIcon img3 = resize_containers("src/Bins/Mixed Paper.jpg");
+        ImageIcon img4 = resize_containers("src/Bins/Organics.jpg");
+        ImageIcon img5 = resize_containers("src/Bins/Plastics.jpg");
+        ImageIcon img6 = resize_containers("src/Bins/Trash.jpg");
+
+        panel.add(new JLabel(img1));
+        panel.add(new JLabel(img2));
+        panel.add(new JLabel(img3));
+        panel.add(new JLabel(img4));
+        panel.add(new JLabel(img5));
+        panel.add(new JLabel(img6));
+    }
+
+
+    //this will resize all the images
+    public ImageIcon resize_containers(String filename){
+        ImageIcon imageIcon = new ImageIcon(filename); // load the image to a imageIcon
+        Image image = imageIcon.getImage(); // transform it
+        Image newimg = image.getScaledInstance(80, 80,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way
+        imageIcon = new ImageIcon(newimg);  // transform it back
+        return imageIcon;
+
+    }
+
+    //sends all info from game setting screen to game info calss
+    public void SetGameGUIParam(String Game_Difficulty, String Game_Item_type , boolean timer, int Game_num_items){
+        G1.set_game_all_param(Game_Difficulty, Game_Item_type ,timer, Game_num_items); //give input to this class
+        G1.pick_Database_word_array(); //this class will select the array
+    }
+
+
     @Override
     public void GameGUI(){}
     @Override
--- src/Game/Game_Info.java
+++ src/Game/Game_Info.java
@@ -1,14 +1,24 @@
 package Game;
 import java.util.*;
 
-public class Game_Info implements Game_parameters{
+public class Game_Info extends Data implements Game_parameters {
     protected int max_number_items=5; // num of items
     protected int counter=0; // counter will record item number
     protected int score=0; // score will record current score
     protected String difficultyLevel= "";
-    protected String theme = "";
     protected String item_types = "";
     protected boolean Timer = false;
+    protected String user_current_word;
+
+    //Which array is selected
+    protected String [] HardDiffLevel_data;
+    protected String [] HardDiffLevel_answer;
+    protected String [] mediumDiffLevel_data;
+    protected String [] mediumDiffLevel_answer;
+    protected String [] EasyDiffLevel_data;
+    protected String [] EasyDiffLevel_answer;
+    protected String Game_Chosen = "";
+    protected String [] answer= {"Bottles/Cans","E-Waste","Mixed Paper","Organics","Plastics","Trash"};
 
 
     protected String[] items = {"Vase", "Cans", "Sandwich", "Punch Bowl","Bong"};
@@ -16,13 +26,38 @@ public class Game_Info implements Game_parameters{
     protected String[] container = {"Glass","Metal","Organic"};
     protected String[] track_of_scores = {"","","","",""}; //record yes for correct, no for wrong
     protected static List<Double> array_full_of_scores; // we want this to be static
+    public Data Database = new Data();
+
 
 
     public Game_Info(){
     }
+
+    //if the user selects word option then this function will pick the database
+    public void pick_Database_word_array(){
+        int size = max_number_items;
+        int arr[] = new int[size];
+        arr = Database.playIndex(max_number_items);
+        if(difficultyLevel.equals("Hard")){
+            HardDiffLevel_data = Database.HardWordData(arr, size);
+            HardDiffLevel_answer = Database.HardAnsData(arr, size);
+            Game_Chosen = "Hard";
+        }
+        else if(difficultyLevel.equals("Normal")){
+            mediumDiffLevel_data = Database.MediumWordData(arr, size);
+             mediumDiffLevel_answer = Database.MediumAnsData(arr, size);
+             Game_Chosen = "Normal";
+        }
+        else{ //difficulty is easy
+             EasyDiffLevel_data = Database.EasyWordData(arr, size);
+             EasyDiffLevel_answer = Database.EasyAnsData(arr, size);
+             Game_Chosen = "Easy";
+        }
+    }
+
+
     //helps with intializing
-    public void set_game_all_param(String param1, String param2, String param3, boolean param4, int param5 ){
-        this.theme = param1; // Theme
+    public void set_game_all_param(String param2, String param3, boolean param4, int param5 ){
         this.item_types= param2;
         this.difficultyLevel = param3;
         this.Timer = param4;
@@ -53,6 +88,21 @@ public class Game_Info implements Game_parameters{
     public String get_difficultyLevel(){ return this.difficultyLevel; }
     public String[] get_containers(){return this.container;}
 
+    public String get_Hard_database_word(int counter_game){
+
+        return HardDiffLevel_data[counter_game];
+    }
+    public String get_Normal_database_word(int counter_game){
+
+        return mediumDiffLevel_data[counter_game];
+    }
+    public String get_Easy_database_word(int counter_game){
+
+        return EasyDiffLevel_data[counter_game];
+    }
+    public String getGame_Chosen(){
+        return Game_Chosen;
+    }
 
     //setters
     public void set_counter(int i){ //i has to be between 0 and 5
@@ -64,6 +114,12 @@ public class Game_Info implements Game_parameters{
     public void set_difficultyLevel(String difficulty_level){ this.difficultyLevel = difficulty_level;}
     public void set_containers(String[] container_array){this.container = container_array;} //not too sure if you can set an array equal to another array in java
     public void set_score_history(double score){array_full_of_scores.add(score);}
+    public void set_Current_user_word(String current_word){
+        this.user_current_word = current_word;
+
+    }
+
+
 
     // useful features
     public void increase_score(){ //i has to be between 0 and 5
@@ -78,6 +134,24 @@ public class Game_Info implements Game_parameters{
     public void reset_score(){ //i has to be between 0 and 5
         this.score = 0;
     }
+    public void check_answer(){
+        if(Game_Chosen.equals("Hard")){
+            if(user_current_word.equals(HardDiffLevel_answer[this.counter])){
+                increase_score();
+            }
+        }
+        if(Game_Chosen.equals("Normal")){
+            if(user_current_word.equals(mediumDiffLevel_answer[this.counter])){
+                increase_score();
+            }
+        }
+        if(Game_Chosen.equals("Easy")){
+            if(user_current_word.equals( EasyDiffLevel_answer[this.counter])){
+                increase_score();
+            }
+        }
+    }
+
     public void reset_score_history(double score){array_full_of_scores.removeAll(array_full_of_scores);}
     public String game_statistics(){
         int sum = 0;
